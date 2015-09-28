e2b1fec6e367fdda0b89d232e8964cd37466e629
Lilian Moraru <lilian.moraru@continental-corporation.com>
2015-09-28 14:10:45 +0300

    Adapt to the latest gcc v0.3.16

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 extern crate gcc;
-use std::default::Default;
 
 fn main() {
-    gcc::compile_library("libhello.a", &Default::default(), &["cpp/hello.cpp"]);
+    gcc::compile_library("libhello.a", &["cpp/hello.cpp"]);
     println!("cargo:rustc-flags=-l dylib=stdc++");
 }
